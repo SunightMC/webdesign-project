65db4749cda0d2016cc07b437b92f7482c0c984b
SunightMC <sungi@fyreblitz.com>
2025-06-19 23:49:06 +0200

    sadsd

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com']
-  },
-  output: 'export',
-  trailingSlash: true,
-  assetPrefix: '/out',
-  basePath: '/out',
+  }
 };
 
 export default nextConfig;
